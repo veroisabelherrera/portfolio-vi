f9c7ff25d0b16b02020f6e72cc09ae54e079fef2
Veronica Isabel <veronica@Veronica-Isabel.local>
2020-03-04 13:00:19 -0300

    link app de talento

--- portfolio-vi/src/components/molecules/AboutDev.js
+++ portfolio-vi/src/components/molecules/AboutDev.js
@@ -11,35 +11,36 @@ import { Link } from 'react-router-dom'
 export class AboutDev extends Component {
     render() {
         return (
-                <div className="about-container">
-                    <div>
-                        <div className="about-title">
-                            <H1><strong>Sobre mi</strong></H1>
-                        </div>
-
-                        <div className="description-container">
-                            <div className="about-h2">
-                                <H2>Mi historia profesional en corto</H2>
-                            </div>
-
-                            <div className="v-divider">
-                                <Divider orientation="vertical" />
-                            </div>
-
-                            <div className="about-text">
-                                <Paragraph>
-                                    Con más de 5 años de experiencia en el mundo del diseño digital
-                                    y con diversas experiencias en la planificación,
-                                    coordinación y desarrollo de proyectos digitales,
-                                    me fui interesando y sumergiendo en el código, curiosidad que me
-                                    llevó a ser parte de Laboratoria, y que hoy en día
-                                    me hace desarrolladora Front-end.
-                        </Paragraph>
-                            </div>
-                        </div>
-
+            <div className="about-container">
+                <div>
+                    <div className="about-title">
+                        <H1><strong>Sobre mi</strong></H1>
                     </div>
+
+                    <div className="description-container">
+                        <div className="about-h2">
+                            <H2>Mi historia profesional en corto</H2>
+                        </div>
+
+                        <div className="v-divider">
+                            <Divider orientation="vertical" />
+                        </div>
+
+                        <div className="about-text">
+                            <Paragraph>
+                                Con más de 5 años de experiencia en el mundo del diseño digital
+                                y con diversas experiencias en la planificación,
+                                coordinación y desarrollo de proyectos digitales,
+                                me fui interesando y sumergiendo en el código, curiosidad que me
+                                llevó a ser parte de Laboratoria, y que hoy en día
+                                me hace desarrolladora Front-end.
+                            </Paragraph>
+                            <a className="link" href="https://app.talento.laboratoria.la/profile/XXm7tW6eRxfzg4QQiaaeZa6RXYX2">Cónoce un poco más</a>
+                        </div>
+                    </div>
+
                 </div>
+            </div>
         )
     }
 }
